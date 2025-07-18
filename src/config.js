dc5ebda65d8378d002ce8258fa4bb9cb20fdc9cc
and703 <andwishyou@gmail.com>
2025-07-18 13:48:48 +0700

    Updated config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,1 @@
-@@ .. @@
-   chrome: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
-   enableUI: true,
-   onLinuxTimeout: 30000,
-+  debugLevel: "info", // error, warn, info, debug, trace
-+  enableFileLogging: false,
-   // gameExePath: "C:/Path/To/LegendsOfIdleon.exe", // Optional: set this to override EXE search on Windows
- };
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
