d3b23cefd0f99ab57b14cd2a8aa1b4ced6e13d0a
and703 <andwishyou@gmail.com>
2025-07-21 11:09:35 +0700

    Apply All Improvements for EXE Build

--- src/config.js
+++ src/config.js
@@ -1,1 +1,11 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+exports.injectorConfig = {
+  injreg: "\\w+\\.ApplicationMain\\s*?=",
+  interceptPattern: "*N.js",
+  showConsoleLog: false,
+  chrome: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+  enableUI: true,
+  onLinuxTimeout: 30000,
+  debugLevel: "info", // error, warn, info, debug, trace
+  enableFileLogging: false, // Save logs to injector.log
+  // gameExePath: "C:/Path/To/LegendsOfIdleon.exe", // Optional: set this to override EXE search on Windows
+};
